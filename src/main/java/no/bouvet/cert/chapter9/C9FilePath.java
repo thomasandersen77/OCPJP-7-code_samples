b67cfe33eef75b8781828ada5ccf45d0a86cf363
gautelyngstad <gautebern@gmail.com>
2013-12-03 13:56:33 +0100

    chapter9

---
+++ src/main/java/no/bouvet/cert/chapter9/C9FilePath.java
@@ -0,0 +1,12 @@
+package no.bouvet.cert.chapter9;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gaute.lyngstad
+ * Date: 03.12.13
+ * Time: 13:40
+ * To change this template use File | Settings | File Templates.
+ */
+public final class C9FilePath {
+    public static final String C9PATH = ".\\src\\main\\java\\no\\bouvet\\cert\\chapter9\\";
+}
