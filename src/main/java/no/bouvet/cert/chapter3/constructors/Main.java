0425490b8def7a309585a579800edcd32fd90f8e
thomasandersen77@gmail.com <jbiD3018>
2013-12-04 14:55:42 +0100

    rename to main

--- src/main/java/no/bouvet/cert/chapter3/constructors/Main.java
+++ src/main/java/no/bouvet/cert/chapter3/constructors/Main.java
@@ -4,14 +4,14 @@ package no.bouvet.cert.chapter3.constructors;
  * This class does not define a constructor, therefor Java creates one for you implicit
  * Created by thomasa on 03.12.13.
  */
-public class DefaultConstructor {
+public class Main {
 
     public static void main(String[] args) {
         /**
          * Since I can instantiate this class without defining a constructor, it is
          * because Java does it for me internally
          */
-        DefaultConstructor constructor = new DefaultConstructor();
+        Main constructor = new Main();
         System.out.println(constructor.toString()); // valid instance of object
     }
 }
