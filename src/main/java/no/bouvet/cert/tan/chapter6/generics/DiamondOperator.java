2fdadfd496c278809fcb93239a6808cd6d40c994
thomasandersen77@gmail.com <jbiD3018>
2013-12-06 18:28:36 +0100

    Some changes to doc on diamond operator

--- src/main/java/no/bouvet/cert/tan/chapter6/generics/DiamondOperator.java
+++ src/main/java/no/bouvet/cert/tan/chapter6/generics/DiamondOperator.java
@@ -9,13 +9,14 @@ import java.util.Map;
  * Created by thomasa on 05.12.13.
  */
 public class DiamondOperator {
+
     public static void main(String[] args) {
-        // compiles, but gives a compiler waring
+
         List<Integer> list = new ArrayList();
         // list.add(new Double(123)); // will not compile
 
-        Map<String, Integer> invalidMap = new HashMap();
-        // invalidMap.put(123, "string"); // will not compile
+        Map<String, Integer> invalidMap = new HashMap<>();
+        invalidMap.put("test", 123); // will compile
 
         // type safe and correct
         List<Integer> typeSafeList = new ArrayList<>();
