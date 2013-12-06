af30ef61a79a834198f58febb8b4dd2327333d40
thomasandersen77@gmail.com <jbiD3018>
2013-12-06 12:45:14 +0100

    renamed DefaultConstructorExample.java

--- src/main/java/no/bouvet/cert/chapter3/constructors/DefaultConstructorExample.java
+++ src/main/java/no/bouvet/cert/chapter3/constructors/DefaultConstructorExample.java
@@ -2,16 +2,17 @@ package no.bouvet.cert.chapter3.constructors;
 
 /**
  * This class does not define a constructor, therefor Java creates one for you implicit
+ * <br/>
  * Created by thomasa on 03.12.13.
  */
-public class Main {
+public class DefaultConstructorExample {
 
     public static void main(String[] args) {
         /**
          * Since I can instantiate this class without defining a constructor, it is
          * because Java does it for me internally
          */
-        Main constructor = new Main();
+        DefaultConstructorExample constructor = new DefaultConstructorExample();
         System.out.println(constructor.toString()); // valid instance of object
     }
 }
