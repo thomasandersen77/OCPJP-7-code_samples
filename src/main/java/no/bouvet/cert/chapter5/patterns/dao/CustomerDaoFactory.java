8d8f796923d772998054b8bf7d7da2f8e8e77993
thomasandersen77@gmail.com <jbiD3018>
2013-12-06 17:01:05 +0100

    chapter5 - Simple DAO pattern with a factory (no transfer object) - javadoc

--- src/main/java/no/bouvet/cert/chapter5/patterns/dao/CustomerDaoFactory.java
+++ src/main/java/no/bouvet/cert/chapter5/patterns/dao/CustomerDaoFactory.java
@@ -4,8 +4,13 @@ package no.bouvet.cert.chapter5.patterns.dao;
  * Created by thomasa on 06.12.13.
  */
 public class CustomerDaoFactory {
-
+    /**
+     * Hard coded implementation
+     *
+     * @return a simple mock implementation of CustomerDao
+     */
     public static CustomerDao getFactory(){
+        // here we can change implementation either via a parameter or property file or hard code it
         return new CustomerDaoMockImpl();
     }
 }
