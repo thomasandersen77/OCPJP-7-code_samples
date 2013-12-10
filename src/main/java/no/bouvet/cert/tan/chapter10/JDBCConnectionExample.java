c66cdc78b53e717bf855bb83833644e779242403
thomasandersen77@gmail.com <jbiD3018>
2013-12-10 16:10:53 +0100

    JDBC example

--- src/main/java/no/bouvet/cert/tan/chapter10/JDBCConnectionExample.java
+++ src/main/java/no/bouvet/cert/tan/chapter10/JDBCConnectionExample.java
@@ -1,8 +1,6 @@
 package no.bouvet.cert.tan.chapter10;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * Created by thomasa on 10.12.13.
@@ -15,11 +13,26 @@ public class JDBCConnectionExample {
         String password = "pass";
 
         try(Connection connection = DriverManager.getConnection(url, user, password)) {
-            System.out.printf("isOpen ? %b%n", !connection.isClosed());
-            connection.close();
-            System.out.printf("isOpen ? %b%n", !connection.isClosed());
+            /*
+            Statement insert = connection.createStatement();
+            insert.execute("insert into contact(firstName, lastName, email, phoneNo) values ('thomas','andersen','thomasa@bouvet.no','97407116')");
+            */
+
+            Statement stm = connection.createStatement();
+            ResultSet resultSet = stm.executeQuery("SELECT * FROM contact");
+            while (resultSet.next()){
+                System.out.printf("%s\t", resultSet.getString(1));
+                System.out.printf("%s\t",resultSet.getString(2));
+                System.out.printf("%s\t",resultSet.getString(3));
+                System.out.printf("%s\t",resultSet.getString(4));
+                System.out.printf("%s\t",resultSet.getString(5));
+                System.out.println();
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+
     }
 }
