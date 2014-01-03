599fa12f19c07d73de85e6201f236a77fa7c5217
thomasandersen77@gmail.com <jbiD3018>
2014-01-03 11:03:17 +0100

    Basic Threading (join and async execution)

---
+++ src/main/java/no/bouvet/cert/tan/chapter13/AsyncThreadingMain.java
@@ -0,0 +1,38 @@
+package no.bouvet.cert.tan.chapter13;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thomasa
+ * Date: 03.01.14
+ * Time: 10:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class AsyncThreadingMain {
+
+    public static void main(String[] args) throws InterruptedException {
+        AsyncThread a1 = new AsyncThread();
+        AsyncThread a2 = new AsyncThread();
+        a1.start();
+        a2.start();
+
+
+
+    }
+
+}
+
+class AsyncThread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println("Starting " + getName());
+        for(int i = 1; i <= 5; i++) {
+            System.out.printf("ThreadName: %s, i=%d%n", getName(), i);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/no/bouvet/cert/tan/chapter13/BasicThreadJoinOperation.java
@@ -0,0 +1,39 @@
+package no.bouvet.cert.tan.chapter13;
+
+/**
+ * Created by thomasa on 20.12.13.
+ */
+public class BasicThreadJoinOperation {
+
+    public static void main(String[] args) {
+
+        Thread counter = new Thread(new CountToTenThread());
+        System.out.println("Start counting to 10... \nCurrentThread: " + Thread.currentThread().getName());
+        counter.start();
+
+        try {
+            counter.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Finished counting...");
+
+    }
+
+}
+
+class CountToTenThread implements Runnable {
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 10; i++) {
+            System.out.printf("%s, %d%n", Thread.currentThread().getName(), i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
