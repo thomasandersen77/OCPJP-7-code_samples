fe5404d434d91f4546ac2dea512efeeb0035d461
thomasandersen77@gmail.com <jbiD3018>
2014-01-03 12:12:49 +0100

    Race condition and synchronized locking of shared resources

---
+++ src/main/java/no/bouvet/cert/tan/chapter13/RaceConditionExampleMain.java
@@ -0,0 +1,46 @@
+package no.bouvet.cert.tan.chapter13;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thomasa
+ * Date: 03.01.14
+ * Time: 11:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class RaceConditionExampleMain {
+
+    public static void main(String[] args) {
+        UserCounter uc = new UserCounter();
+        Thread t1 = new Thread(uc);
+        Thread t2 = new Thread(uc);
+        Thread t3 = new Thread(uc);
+        t1.start();
+        t2.start();
+        t3.start();
+
+
+    }
+
+}
+
+class Counter {
+    public static int count;
+}
+
+class UserCounter implements Runnable{
+
+    public void increment() {
+        // this avoids race condition / data race
+        synchronized (this) {
+            Counter.count++;
+            System.out.print(Counter.count  + " ");
+        }
+    }
+
+    @Override
+    public void run() {
+        increment();
+        increment();
+        increment();
+    }
+}
